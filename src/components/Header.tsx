9d00c4718f92a9b490b94feb39d15bdb4e39145b
viniciusperrone <63932366+viniciusperrone@users.noreply.github.com>
2021-04-24 15:50:41 -0400

    add initial header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,28 @@ import {
     Image
 } from 'react-native';
 
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/vinicius.jpg';
+
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 export function Header(){
     return(
         <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>
+                    Olá,
+                </Text>
+                <Text style={styles.userName}>
+                    Vinicius
+                </Text>
+            </View>
+            <Image 
+                source={userImg} 
+                style={styles.image}
+            />
         </View>
     )
 }
@@ -23,7 +40,23 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingVertical: 20,
-        backgroundColor: colors.red
-
+        marginTop: getStatusBarHeight(),
+        padding: 20
+    },
+    image: {
+        width: 80,
+        height: 80,
+        borderRadius: 40
+    },
+    greeting: {
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName: {
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 48
     }
 })
